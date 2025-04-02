64357145b24413cf6c53e37a1699c41202487f5a
Dudam Neeraj Dattu <dudamdattu@gmail.com>
2025-04-01 23:18:07 -0500

    tried to add music play functionality

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 console.log('Script loaded successfully!');
 
-function toggleMenu() {
+let library = {};
+let currentPlaylist = '';
+let isPlaying = false;
+let currentSong = new Audio();
+let currentSongIndex = 0;
+let currentSongName = '';
+let started = false;
+
+const toggleMenu = () => {
     const open = document.querySelector('.open');
 
     const close = document.querySelector('.close');
@@ -16,15 +24,42 @@ function toggleMenu() {
     });
 }
 
-const getPlaylists = async () => { 
+const getPlaylists = async () => {
     let a = await fetch('http://127.0.0.1:3000/songs/')
     let text = await a.text()
     // console.log(text);
     let div = document.createElement("div");
     div.innerHTML = text;
-    let links = div.querySelectorAll('td a');    
+    let links = div.querySelectorAll('td a');
     let folderNames = Array.from(links).map(a => a.textContent.replace("/", "")).slice(1);
     return folderNames;
+}
+
+const libraryObject = async (playlistName) => {
+    let songs = await fetch(`http://127.0.0.1:3000/songs/${playlistName}`);
+    let text = await songs.text()
+    // console.log(text);
+    let div = document.createElement("div");
+    div.innerHTML = text;
+    let links = div.querySelectorAll('td a');
+    let songNames = Array.from(links).map(a => a.textContent.replace("/", "")).slice(1);
+
+    songNames.sort((a, b) => {
+        let numA = parseInt(a.split('.')[0]);
+        let numB = parseInt(b.split('.')[0]);
+        return numA - numB;
+    });
+
+    let songlist = [];
+    let songsObject = {};
+    songNames.forEach(name => {
+        let songName = name.split('.')[1].slice(1);
+        let songFileName = name.replaceAll(" ", "%20");
+        let audioSrcUrl = `http://127.0.0.1:3000/songs/${playlistName}/${songFileName}`;
+        songsObject[songName] = audioSrcUrl;
+        songlist.push(songName);
+    });
+    return songsObject;
 }
 
 const populatePlaylists = async (names) => {
@@ -47,6 +82,15 @@ const populatePlaylists = async (names) => {
         playlistElement.appendChild(div);
     });
 
+    for (const name of names) {
+        library[name] = await libraryObject(name);
+    }
+
+    currentPlaylist = names[0];
+    currentSongIndex = 0;
+    currentSongName = Object.keys(library[currentPlaylist])[currentSongIndex];
+    currentSong = new Audio(Object.values(library[currentPlaylist])[currentSongIndex]);
+    console.log(library);
     console.log('Playlists populated successfully.');
 };
 
@@ -96,51 +140,95 @@ const populateSongs = async (songs) => {
         // Append <li> to <ul>
         ulElement.appendChild(li);
     });
-    
-    console.log('Songs populated successfully.');
+
+    // console.log('Songs populated successfully.');
 };
 
-const updateSeekBar = async (song) => { 
-    let songDisplay = document.querySelector('.songName h3');
-    songDisplay.innerHTML = song
-    
-    
-    
-}
-const defaultSongsLoader = async (playlistName) => {
-    let songs = await fetch(`http://127.0.0.1:3000/songs/${playlistName}`);
-    let text = await songs.text()
-    // console.log(songsText);
-    let div = document.createElement("div");
-    div.innerHTML = text;
-    let links = div.querySelectorAll('td a');    
-    let songNames = Array.from(links).map(a => a.textContent.replace("/", "")).slice(1);
+const secondsToMinutesSeconds = (seconds) => {
+    if (isNaN(seconds) || seconds < 0) {
+        return "00:00";
+    }
 
-    songNames.sort((a, b) => {
-        let numA = parseInt(a.split('.')[0]);
-        let numB = parseInt(b.split('.')[0]); 
-        return numA - numB; 
-    });
-    
-    console.log(songNames);
-    
-    songlist = [];
-    songNames.forEach(name => {
-        songlist.push(name.split('.')[1].slice(1));
-    });
-    console.log(songlist);
-    await populateSongs(songlist);  
-    await updateSeekBar(songlist[0]);
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
 }
 
 
-async function main() {
-    const folderNames = await getPlaylists();    
-    console.log(folderNames);
+const main = async () => {
+    const folderNames = await getPlaylists();
+    // console.log(folderNames);
     await populatePlaylists(folderNames);
-    await defaultSongsLoader(folderNames[0]);
-    // await populateSongs(songs);
+    await populateSongs(Object.keys(library[folderNames[0]]));
+
+    //toggling the playlist populates the songs in the list
+    let togglePlaylist = document.querySelectorAll('.playlist');
+
+    togglePlaylist.forEach(playlist => {
+        playlist.addEventListener('click', async () => {
+            let playlistName = playlist.textContent;
+            await populateSongs(Object.keys(library[playlistName]));
+
+            currentPlaylist = playlist;
+            currentSongIndex = 0;
+            if (currentSong) {
+                currentSong.pause();
+                currentSong.currentTime = 0;
+            }
+            currentSong = new Audio(Object.values(songsObject)[currentSongIndex]);
+
+            currentSongName = Object.keys(songsObject)[currentSongIndex];
+
+            console.log(`Current song: ${currentSongName}`, `Current playlist: ${currentPlaylist}`);
+
+            let songDisplay = document.querySelector('.songName h3');
+            songDisplay.innerHTML = currentSongName;
+        });
+    });
+
+
+    let togglePlay = document.querySelector('.controlIcons img:nth-child(2)');
+    togglePlay.addEventListener('click', async () => {
+        if (!isPlaying) {
+            currentSong.play();
+            isPlaying = true;
+            started = true;
+            togglePlay.src = 'img/pause.svg';
+        }
+        else if (isPlaying) {
+            currentSong.pause();
+            isPlaying = false;
+            togglePlay.src = 'img/play.svg';
+        }
+    });
+
+    // Listen for timeupdate event
+    currentSong.addEventListener("timeupdate", () => {
+        document.querySelector(".timeStamp h3").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)} / ${secondsToMinutesSeconds(currentSong.duration)}`;
+
+        let percent = (currentSong.currentTime / currentSong.duration) * 100;
+        document.querySelector(".circle").style.left = percent + "%";
+        if (currentSong.currentTime === currentSong.duration) {
+            isPlaying = false;
+            togglePlay.src = 'img/play.svg';
+        }
+    });
+
+    // Add an event listener to seekbar
+    document.querySelector(".seekbar").addEventListener("click", e => {
+        let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
+        document.querySelector(".circle").style.left = percent + "%";
+        currentSong.currentTime = ((currentSong.duration) * percent) / 100
+    })
+
+
 }
 
 toggleMenu();
 main();
+
+
